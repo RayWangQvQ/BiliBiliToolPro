a9798475d3466c92ef60fd0cb11a3dcbfbb82fc1
RayWang <2271272653@qq.com>
2020-11-21 17:41:19 +0800

    针对B站近期风控进行调整 1.将User-Agent开放为配置，解决当前出现登录报423的问题； 2.新增一个是否跳过每日任务的配置，用于灵活的关闭、开启每日任务。

--- src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Interfaces/IBiliBiliApi.cs
+++ src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Interfaces/IBiliBiliApi.cs
@@ -5,8 +5,7 @@ namespace Ray.BiliBiliTool.Agent.BiliBiliAgent.Interfaces
     [Headers(
     "Accept:application/json, text/plain, */*",
     "Referer:https://www.bilibili.com/",
-    "Connection:keep-alive",
-    "User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 Edg/85.0.564.70"
+    "Connection:keep-alive"
     )]
     public interface IBiliBiliApi
     {
