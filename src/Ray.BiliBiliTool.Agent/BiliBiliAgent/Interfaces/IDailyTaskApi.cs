b6274d84a19d815273462b8004e667da3c954883
hjkl950217 <584880422@qq.com>
2020-12-08 16:31:20 +0800

    迁移回代码，排除生成代理报错

--- src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Interfaces/IDailyTaskApi.cs
+++ src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Interfaces/IDailyTaskApi.cs
@@ -41,6 +41,43 @@ namespace Ray.BiliBiliTool.Agent.BiliBiliAgent.Interfaces
         [Get("/x/space/arc/search?mid={upId}&ps={pageSize}&tid=0&pn={pageNumber}&keyword={keyword}&order=pubdate&jsonp=jsonp")]
         Task<BiliApiResponse<SearchUpVideosResponse>> SearchVideosByUpId(long upId, int pageSize = 20, int pageNumber = 1, string keyword = "");
 
+        #region 用户信息
+
+        /// <summary>
+        /// 获取每日任务的完成情况
+        /// </summary>
+        /// <returns></returns>
+        [Get("/x/member/web/exp/reward")]
+        Task<BiliApiResponse<DailyTaskInfo>> GetDailyTaskRewardInfo();
+
+        /// <summary>
+        /// 获取通过投币已获取的经验值
+        /// </summary>
+        /// <returns></returns>
+        [Get("/x/web-interface/coin/today/exp")]
+        Task<BiliApiResponse<int>> GetDonateCoinExp();
+
+        /// <summary>
+        /// 获取VIP特权
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="csrf"></param>
+        /// <returns></returns>
+        [Post("/x/vip/privilege/receive?type={type}&csrf={csrf}")]
+        Task<BiliApiResponse> ReceiveVipPrivilege(int type, string csrf);
+
+        /// <summary>
+        /// 获取当前用户对<paramref name="aid"/>视频的投币信息
+        /// </summary>
+        /// <param name="aid"></param>
+        /// <returns></returns>
+        [Get("/x/web-interface/archive/coins?aid={aid}")]
+        Task<BiliApiResponse<DonatedCoinsForVideo>> GetDonatedCoinsForVideo(string aid);
+
+        #endregion 用户信息
+
+        #region 用户操作
+
         /// <summary>
         /// 为视频投币
         /// </summary>
@@ -51,5 +88,52 @@ namespace Ray.BiliBiliTool.Agent.BiliBiliAgent.Interfaces
         /// <returns></returns>
         [Post("/x/web-interface/coin/add?aid={aid}&multiply={multiply}&select_like={select_like}&cross_domain=true&csrf={csrf}")]
         Task<BiliApiResponse> AddCoinForVideo(string aid, int multiply, int select_like, string csrf);
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <returns></returns>
+        [Get("/x/web-interface/nav")]
+        Task<BiliApiResponse<UseInfo>> LoginByCookie();
+
+        /// <summary>
+        /// 分享视频
+        /// </summary>
+        /// <param name="aid"></param>
+        /// <param name="csrf"></param>
+        /// <returns></returns>
+        [Post("/x/web-interface/share/add?aid={aid}&csrf={csrf}")]
+        Task<BiliApiResponse> ShareVideo(string aid, string csrf);
+
+        /// <summary>
+        /// 上传视频观看进度
+        /// </summary>
+        /// <returns></returns>
+        [Post("/x/click-interface/web/heartbeat?aid={aid}&played_time={playedTime}")]
+        Task<BiliApiResponse> UploadVideoHeartbeat(string aid, int playedTime);
+
+        /// <summary>
+        /// 充电
+        /// </summary>
+        /// <param name="elec_num"></param>
+        /// <param name="up_mid"></param>
+        /// <param name="oid"></param>
+        /// <param name="csrf"></param>
+        /// <returns></returns>
+        [Post("/x/ugcpay/trade/elec/pay/quick?elec_num={elec_num}&up_mid={up_mid}&otype=up&oid={oid}&csrf={csrf}")]
+        Task<BiliApiResponse<ChargeResponse>> Charge(decimal elec_num, string up_mid, string oid, string csrf);
+
+        /// <summary>
+        /// 充电后留言
+        /// </summary>
+        /// <param name="elec_num"></param>
+        /// <param name="up_mid"></param>
+        /// <param name="oid"></param>
+        /// <param name="csrf"></param>
+        /// <returns></returns>
+        [Post("/x/ugcpay/trade/elec/message?order_id={order_id}&message={message}&csrf={csrf}")]
+        Task<BiliApiResponse<ChargeResponse>> ChargeComment(string order_id, string message, string csrf);
+
+        #endregion 用户操作
     }
 }
--- src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Interfaces/IUserOperationApi.cs
+++ src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Interfaces/IUserOperationApi.cs
@@ -1,57 +1,9 @@
-﻿using System.Threading.Tasks;
-using Ray.BiliBiliTool.Agent.BiliBiliAgent.Dtos;
-using Refit;
-
-namespace Ray.BiliBiliTool.Agent.BiliBiliAgent.Interfaces
+﻿namespace Ray.BiliBiliTool.Agent.BiliBiliAgent.Interfaces
 {
     /// <summary>
     /// 用户操作API
     /// </summary>
     public interface IUserOperationApi
     {
-        /// <summary>
-        /// 登录
-        /// </summary>
-        /// <returns></returns>
-        [Get("/x/web-interface/nav")]
-        Task<BiliApiResponse<UseInfo>> LoginByCookie();
-
-        /// <summary>
-        /// 分享视频
-        /// </summary>
-        /// <param name="aid"></param>
-        /// <param name="csrf"></param>
-        /// <returns></returns>
-        [Post("/x/web-interface/share/add?aid={aid}&csrf={csrf}")]
-        Task<BiliApiResponse> ShareVideo(string aid, string csrf);
-
-        /// <summary>
-        /// 上传视频观看进度
-        /// </summary>
-        /// <returns></returns>
-        [Post("/x/click-interface/web/heartbeat?aid={aid}&played_time={playedTime}")]
-        Task<BiliApiResponse> UploadVideoHeartbeat(string aid, int playedTime);
-
-        /// <summary>
-        /// 充电
-        /// </summary>
-        /// <param name="elec_num"></param>
-        /// <param name="up_mid"></param>
-        /// <param name="oid"></param>
-        /// <param name="csrf"></param>
-        /// <returns></returns>
-        [Post("/x/ugcpay/trade/elec/pay/quick?elec_num={elec_num}&up_mid={up_mid}&otype=up&oid={oid}&csrf={csrf}")]
-        Task<BiliApiResponse<ChargeResponse>> Charge(decimal elec_num, string up_mid, string oid, string csrf);
-
-        /// <summary>
-        /// 充电后留言
-        /// </summary>
-        /// <param name="elec_num"></param>
-        /// <param name="up_mid"></param>
-        /// <param name="oid"></param>
-        /// <param name="csrf"></param>
-        /// <returns></returns>
-        [Post("/x/ugcpay/trade/elec/message?order_id={order_id}&message={message}&csrf={csrf}")]
-        Task<BiliApiResponse<ChargeResponse>> ChargeComment(string order_id, string message, string csrf);
     }
 }
