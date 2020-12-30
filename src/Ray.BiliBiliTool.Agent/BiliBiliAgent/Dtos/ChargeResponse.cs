ac3549eaabd185a73785fba4e5a379e939bb216b
WangRui <2271272653@qq.com>
2020-12-30 23:24:04 +0800

    更新充电接口

--- src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Dtos/ChargeResponse.cs
+++ src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Dtos/ChargeResponse.cs
@@ -6,4 +6,21 @@
 
         public string Order_no { get; set; }
     }
+
+    public class ChargeV2Response
+    {
+        public string Bp_num { get; set; }
+
+        public decimal Exp { get; set; }
+
+        public long Mid { get; set; }
+
+        public string Msg { get; set; }
+
+        public string Order_no { get; set; }
+
+        public int Status { get; set; }
+
+        public int Up_mid { get; set; }
+    }
 }
--- src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Interfaces/IDailyTaskApi.cs
+++ src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Interfaces/IDailyTaskApi.cs
@@ -124,6 +124,17 @@ namespace Ray.BiliBiliTool.Agent.BiliBiliAgent.Interfaces
         Task<BiliApiResponse<ChargeResponse>> Charge(int elec_num, string up_mid, string oid, string csrf);
 
         /// <summary>
+        /// 充电V2
+        /// </summary>
+        /// <param name="bp_num">B币个数</param>
+        /// <param name="up_mid">对方Id</param>
+        /// <param name="oid">对方来源代码(空间充电：充电对象用户UID;视频充电：稿件avID)</param>
+        /// <param name="csrf">自己的bili_jct</param>
+        /// <returns></returns>
+        [Post("/x/ugcpay/web/v2/trade/elec/pay/quick?is_bp_remains_prior=true&bp_num={bp_num}&up_mid={up_mid}&otype=up&oid={oid}&csrf={csrf}")]
+        Task<BiliApiResponse<ChargeV2Response>> ChargeV2(decimal bp_num, string up_mid, string oid, string csrf);
+
+        /// <summary>
         /// 充电后留言
         /// </summary>
         /// <param name="elec_num"></param>
