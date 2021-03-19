3a4507dea905e8a149ce520c5c4d4bb028aa70df
RayWang <2271272653@qq.com>
2021-03-19 17:56:12 +0800

    删除过时的Cookie配置方式

--- src/Ray.BiliBiliTool.Config/Options/BiliBiliCookieOptions.cs
+++ src/Ray.BiliBiliTool.Config/Options/BiliBiliCookieOptions.cs
@@ -7,29 +7,5 @@ namespace Ray.BiliBiliTool.Config.Options
     public class BiliBiliCookieOptions
     {
         public string CookieStr { get; set; }
-
-        /// <summary>
-        /// DedeUserID
-        /// </summary>
-        public string UserId { get; set; }
-
-        public string DedeUserID { get; set; }
-
-        /// <summary>
-        /// SESSDATA
-        /// </summary>
-        public string SessData { get; set; }
-
-        /// <summary>
-        /// bili_jct
-        /// </summary>
-        public string BiliJct { get; set; }
-
-        public string Bili_jct { get; set; }
-
-        /// <summary>
-        /// 其他Cookies
-        /// </summary>
-        public string OtherCookies { get; set; }
     }
 }
