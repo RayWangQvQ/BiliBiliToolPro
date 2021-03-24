e675b35ed2e485dd870465048f0354df0ba685f5
RayWang <2271272653@qq.com>
2021-03-24 22:56:15 +0800

    解决压缩导致接口返回乱码问题 #523

--- src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Interfaces/IBiliBiliApi.cs
+++ src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Interfaces/IBiliBiliApi.cs
@@ -5,7 +5,7 @@ namespace Ray.BiliBiliTool.Agent.BiliBiliAgent.Interfaces
 {
     [Header("Accept", "application/json, text/plain, */*")]
     //[Header("Accept-Encoding", "gzip, deflate, br")]
-    [Header("Accept-Encoding", "deflate, br")]
+    //[Header("Accept-Encoding", "deflate, br")]
     [Header("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6")]
 
     [Header("Sec-Fetch-Dest", "empty")]
