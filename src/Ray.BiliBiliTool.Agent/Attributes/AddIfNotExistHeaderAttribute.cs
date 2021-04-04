51eb483b2adac78c03219c55b97d692d5d925f84
RayWang <2271272653@qq.com>
2021-04-04 23:56:36 +0800

    新增Header特性，用于子类直接替换基类的Header，而不是附加

---
+++ src/Ray.BiliBiliTool.Agent/Attributes/AddIfNotExistHeaderAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiClientCore;
+using WebApiClientCore.Attributes;
+
+namespace Ray.BiliBiliTool.Agent.Attributes
+{
+    public class AddIfNotExistHeaderAttribute : HeaderAttribute
+    {
+        private readonly string _name;
+        private readonly string _value;
+
+        public AddIfNotExistHeaderAttribute(string name, string value) : base(name, value)
+        {
+            this._name = name;
+            this._value = value;
+        }
+
+        public AddIfNotExistHeaderAttribute(string aliasName) : base(aliasName)
+        {
+
+        }
+
+        public override Task OnRequestAsync(ApiRequestContext context)
+        {
+            //因为逻辑是先添加子类的，再加基类的。所以当子类已经添加过了，基类就不加了
+            if (context.HttpContext.RequestMessage.Headers.Contains(this._name))
+                return Task.CompletedTask;
+
+            return base.OnRequestAsync(context);
+        }
+    }
+}
--- src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Interfaces/IBiliBiliApi.cs
+++ src/Ray.BiliBiliTool.Agent/BiliBiliAgent/Interfaces/IBiliBiliApi.cs
@@ -3,16 +3,15 @@ using WebApiClientCore.Attributes;
 
 namespace Ray.BiliBiliTool.Agent.BiliBiliAgent.Interfaces
 {
-    [Header("Accept", "application/json, text/plain, */*")]
-    //[Header("Accept-Encoding", "gzip, deflate, br")]
-    //[Header("Accept-Encoding", "deflate, br")]
-    [Header("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6")]
+    [AddIfNotExistHeader("Accept", "application/json, text/plain, */*")]
+    //[AddIfNotExistHeader("Accept-Encoding", "gzip, deflate, br")]
+    [AddIfNotExistHeader("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6")]
 
-    [Header("Sec-Fetch-Dest", "empty")]
-    [Header("Sec-Fetch-Mode", "cors")]
-    [Header("Sec-Fetch-Site", "same-site")]
+    [AddIfNotExistHeader("Sec-Fetch-Dest", "empty")]
+    [AddIfNotExistHeader("Sec-Fetch-Mode", "cors")]
+    [AddIfNotExistHeader("Sec-Fetch-Site", "same-site")]
 
-    [Header("Connection", "keep-alive")]
+    [AddIfNotExistHeader("Connection", "keep-alive")]
 
     [LogFilter]
     public interface IBiliBiliApi
