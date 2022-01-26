fae81aabce983cafc4f1419715581e903bed3b38
Ray <zai7lou@outlook.com>
2022-01-26 23:06:47 +0800

    Update

--- qinglong/DefaultTasks/bili_task_daily.sh
+++ qinglong/DefaultTasks/bili_task_daily.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # bili每日任务
-# cron 0 9 * * * bili_task_test.sh
+# cron 0 9 * * * bili_task_daily.sh
 
 dotnet run --project /ql/repo/raywangqvq_bilibilitoolpro/src/Ray.BiliBiliTool.Console Production -runTasks=Daily
