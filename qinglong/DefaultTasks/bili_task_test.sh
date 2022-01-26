26e8aea9319f4c3767dc3436e952fa372cc93b2c
Ray Wang <ray.wang@batechworks.com>
2022-01-26 16:39:34 +0800

    Update

--- qinglong/DefaultTasks/bili_task_test.sh
+++ qinglong/DefaultTasks/bili_task_test.sh
@@ -2,4 +2,4 @@
 
 # cron 0 0 * * * bili_task_test.sh
 
-dotnet run --project /ql/repo/raywangqvq_bilibilitoolpro/src/Ray.BiliBiliTool.Console -runTasks=Daily
+dotnet run --project /ql/repo/raywangqvq_bilibilitoolpro/src/Ray.BiliBiliTool.Console -runTasks=Test
