7fdd7d68f70c13a7cbd8998c1d90f678203aade3
Ray <zai7lou@outlook.com>
2022-03-31 22:58:48 +0800

    feat:[#47]Support arm to install dotnet

--- qinglong/extra.sh
+++ qinglong/extra.sh
@@ -1,31 +1,8 @@
 ## 添加你需要重启自动执行的任意命令，比如 ql repo
-###
- # @Author: your name
- # @Date: 2022-01-26 20:24:25
- # @LastEditTime: 2022-01-26 22:47:41
- # @LastEditors: Please set LastEditors
- # @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- # @FilePath: \BiliBiliToolPro\qinglong\extra.sh
-### 
 ## 安装node依赖使用 pnpm install -g xxx xxx
 ## 安装python依赖使用 pip3 install xxx
 
 # 安装 dotnet 环境
-echo -e "\n-------set up dot net env-------"
-apk add bash icu-libs krb5-libs libgcc libintl libssl1.1 libstdc++ zlib
-wget https://download.visualstudio.microsoft.com/download/pr/bd94779d-c7c4-47fd-b80a-0088caa0afc6/40f115bbf4c068359e7a066fe0b03dbc/dotnet-sdk-6.0.101-linux-musl-x64.tar.gz
-wget https://download.visualstudio.microsoft.com/download/pr/bd94779d-c7c4-47fd-b80a-0088caa0afc6/40f115bbf4c068359e7a066fe0b03dbc/dotnet-sdk-6.0.101-linux-musl-x64.tar.gz
-
-
-https://download.visualstudio.microsoft.com/download/pr/72888385-910d-4ef3-bae2-c08c28e42af0/59be90572fdcc10766f1baf5ac39529a/dotnet-sdk-6.0.101-linux-arm.tar.gz
-
-
-DOTNET_FILE=dotnet-sdk-6.0.101-linux-musl-x64.tar.gz
-export DOTNET_ROOT=/home/dotnet
-mkdir -p "$DOTNET_ROOT" && tar zxf "$DOTNET_FILE" -C "$DOTNET_ROOT"
-export PATH=$PATH:$DOTNET_ROOT
-ln -s /home/dotnet/dotnet /usr/local/bin
-dotnet --version
-echo -e "\n-------set up dot net env finish-------"
+bash <(curl -sSL https://raw.githubusercontent.com/RayWangQvQ/BiliBiliToolPro/main/qinglong/ray-dotnet-install.sh)
 
 # 其他代码...
