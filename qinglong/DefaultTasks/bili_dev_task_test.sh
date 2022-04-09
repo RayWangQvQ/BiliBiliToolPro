6f70b5e693b4eb6c1a66c53fa4ab7cd63a4252b0
Ray <zai7lou@outlook.com>
2022-04-10 00:05:13 +0800

    feat:[#54]Optimize the way of reading qinglong repo directories

--- qinglong/DefaultTasks/bili_dev_task_test.sh
+++ qinglong/DefaultTasks/bili_dev_task_test.sh
@@ -2,7 +2,14 @@
 # new Env("bili测试ck[dev先行版]")
 # cron 0 8 * * * bili_dev_task_test.sh
 
-cd "$(find /ql -type d -name "repo" -print)"
-cd "$(find . -type d -name "raywangqvq_bilibilitoolpro_develop" -print)"
+dir_shell=$QL_DIR/shell
+. $dir_shell/share.sh
 
+bili_repo="raywangqvq_bilibilitoolpro_develop"
+
+echo "repo目录: $dir_repo"
+bili_repo_dir="$(find $dir_repo -type d -name $bili_repo | head -1)"
+echo -e "bili仓库目录: $bili_repo_dir\n"
+
+cd $bili_repo_dir
 dotnet run --project ./src/Ray.BiliBiliTool.Console --ENVIRONMENT=Production --runTasks=Test
--- qinglong/DefaultTasks/bili_task_daily.sh
+++ qinglong/DefaultTasks/bili_task_daily.sh
@@ -2,7 +2,14 @@
 # new Env("bili每日任务")
 # cron 0 9 * * * bili_task_daily.sh
 
-cd "$(find /ql -type d -name "repo" -print)"
-cd "$(find . -type d -name "raywangqvq_bilibilitoolpro" -print)"
+dir_shell=$QL_DIR/shell
+. $dir_shell/share.sh
 
+bili_repo="raywangqvq_bilibilitoolpro"
+
+echo "repo目录: $dir_repo"
+bili_repo_dir="$(find $dir_repo -type d -name $bili_repo | head -1)"
+echo -e "bili仓库目录: $bili_repo_dir\n"
+
+cd $bili_repo_dir
 dotnet run --project ./src/Ray.BiliBiliTool.Console --ENVIRONMENT=Production --runTasks=Daily
