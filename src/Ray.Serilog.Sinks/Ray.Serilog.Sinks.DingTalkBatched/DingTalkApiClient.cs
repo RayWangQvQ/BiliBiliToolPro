3c2f45bb0ccb26f17f43ee9787440dd10bcef866
scoful.tang <Infox123456>
2022-04-11 18:55:16 +0800

    增加钉钉发送消息时加密, issue : [#94]

--- src/Ray.Serilog.Sinks/Ray.Serilog.Sinks.DingTalkBatched/DingTalkApiClient.cs
+++ src/Ray.Serilog.Sinks/Ray.Serilog.Sinks.DingTalkBatched/DingTalkApiClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Ray.Serilog.Sinks.Batched;
@@ -15,7 +16,14 @@ namespace Ray.Serilog.Sinks.DingTalkBatched
 
         public DingTalkApiClient(string webHookUrl)
         {
-            _apiUrl = new Uri(webHookUrl);
+            if (null != webHookUrl && webHookUrl.Contains("secret="))
+            {
+                _apiUrl = new Uri(ToGetSignUrl(webHookUrl));
+            }
+            else
+            {
+                _apiUrl = new Uri(webHookUrl);
+            }
         }
 
         public override string ClientName => "钉钉机器人";
@@ -49,6 +57,32 @@ namespace Ray.Serilog.Sinks.DingTalkBatched
 
             var response = _httpClient.PostAsync(_apiUrl, content).GetAwaiter().GetResult();
             return response;
+        }
+        // hmac256加密并返回base64
+        public static string ToBase64hmac(string strText, string strKey)
+        {
+            HMACSHA256 myHMACSHA256 = new HMACSHA256(Encoding.UTF8.GetBytes(strKey));
+            byte[] byteText = myHMACSHA256.ComputeHash(Encoding.UTF8.GetBytes(strText));
+            return System.Convert.ToBase64String(byteText);
+        }
+        // 推送钉钉消息url加上签名
+        public static string ToGetSignUrl(string webHookUrl)
+        {
+            var secret = "";
+            var temp = webHookUrl.Replace("https://oapi.dingtalk.com/robot/send?", "");
+            string[] vs = temp.Split("&");
+            for (int i = 0; i < vs.Length; i++)
+            {
+                if (vs[i].StartsWith("secret="))
+                {
+                    secret = vs[i].Replace("secret=", "");
+                    break;
+                }
+            }
+            var current = (DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000;
+            var string_to_sign = current + "\n" + secret;
+            var sign = ToBase64hmac(string_to_sign, secret);
+            return webHookUrl + "&timestamp=" + current + "&sign=" + sign;
         }
     }
 
