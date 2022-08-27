040cdad09a0bfbe753184c5b6e46c57609701389
syrinka <syrinka@foxmail.com>
2022-08-27 22:59:35 +0800

    fix

--- docker/entry.sh
+++ docker/entry.sh
@@ -8,7 +8,8 @@ declare -p | grep -v "no_proxy" > /etc/cron.env
 echo "=>完成"
 
 echo "[step 2/4]配置cron定时任务"
-echo "BASH_ENV=/etc/cron.env" > /etc/cron.d/bilicron
+echo "SHELL=/bin/bash" > /etc/cron.d/bilicron
+echo "BASH_ENV=/etc/cron.env" >> /etc/cron.d/bilicron
 if [ -z "$Ray_DailyTaskConfig__Cron$Ray_LiveLotteryTaskConfig__Cron$Ray_UnfollowBatchedTaskConfig__Cron$Ray_VipBigPointConfig__Cron" ]; then
 	echo "=>使用默认的定时任务配置"
 	cat /app/crontab >> /etc/cron.d/bilicron
@@ -35,7 +36,8 @@ touch /var/log/cron.log
 
 if ! [ -z "$Ray_Crontab" ]; then
 	echo "=>检测到自定义定时任务"
-	echo "BASH_ENV=/etc/cron.env" > /etc/cron.d/customcron
+	echo "SHELL=/bin/bash" > /etc/cron.d/customcron
+	echo "BASH_ENV=/etc/cron.env" >> /etc/cron.d/customcron
 	echo "$Ray_Crontab" >> /etc/cron.d/customcron
 
 	cat /etc/cron.d/customcron
