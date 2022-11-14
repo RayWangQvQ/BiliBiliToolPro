e1902afe78b66c55a41ca57f0b0c5115de299661
Ray <zai7lou@outlook.com>
2022-11-14 20:20:30 +0800

    feat:update sh fmt

--- qinglong/DefaultTasks/bili_task_base.sh
+++ qinglong/DefaultTasks/bili_task_base.sh
@@ -14,10 +14,11 @@ dotnet --version
 
 bili_repo="raywangqvq_bilibilitoolpro_develop"
 
-echo "repo目录: $dir_repo"
+echo -e "\nrepo目录: $dir_repo"
 bili_repo_dir="$(find $dir_repo -type d -iname $bili_repo | head -1)"
 echo -e "bili仓库目录: $bili_repo_dir\n"
 
-echo -e "清理缓存"
+echo -e "清理缓存...\n"
 cd $bili_repo_dir
-find . -type d -name "bin" -o -type d -name "obj" -exec rm -rf {} +
+find . -type d -name "bin" -exec rm -rf {} +
+find . -type d -name "obj" -exec rm -rf {} +
