81c41123b1f1dd8d752f83066254bfb8886573b7
aful <liangfu_zhang@qq.com>
2022-12-08 14:46:27 +0800

    [New Feature] 通过扫码更新第一个cookie
    通过将B站登录二维码打印到日志文件中，手机B站app扫描二维码，自动更新第一个Cookie

--- qinglong/extra.sh
+++ qinglong/extra.sh
@@ -5,4 +5,6 @@
 # 安装 dotnet 环境
 # curl -sSL https://ghproxy.com/https://raw.githubusercontent.com/RayWangQvQ/BiliBiliToolPro/main/qinglong/ray-dotnet-install.sh | bash /dev/stdin --no-official
 curl -sSL https://ghproxy.com/https://raw.githubusercontent.com/RayWangQvQ/BiliBiliToolPro/main/qinglong/ray-dotnet-install.sh | bash /dev/stdin
+# 安装需要用到的包
+pip3 install requests qrcode pillow
 # 其他代码...
