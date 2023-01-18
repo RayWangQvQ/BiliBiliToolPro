98a60edbd917f961409b9a282f60882a870b5c30
Ray <zai7lou@outlook.com>
2023-01-18 22:54:33 +0800

    chore: update

--- src/Ray.BiliBiliTool.Application/LiveFansMedalAppService.cs
+++ src/Ray.BiliBiliTool.Application/LiveFansMedalAppService.cs
@@ -1,51 +1,48 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Logging;
 using Ray.BiliBiliTool.Application.Attributes;
 using Ray.BiliBiliTool.Application.Contracts;
-using Ray.BiliBiliTool.Config.Options;
 using Ray.BiliBiliTool.DomainService.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ray.BiliBiliTool.Application
 {
     public class LiveFansMedalAppService : AppService, ILiveFansMedalAppService
     {
         private readonly ILogger<LiveFansMedalAppService> _logger;
-        private readonly IConfiguration _configuration;
         private readonly ILiveDomainService _liveDomainService;
-        private readonly LiveLotteryTaskOptions _liveLotteryTaskOptions;
-        private readonly SecurityOptions _securityOptions;
-        private readonly IAccountDomainService _accountDomainService;
 
         public LiveFansMedalAppService(
-            IConfiguration configuration,
             ILiveDomainService liveDomainService,
-            IOptionsMonitor<SecurityOptions> securityOptions,
-            IOptionsMonitor<LiveLotteryTaskOptions> liveLotteryTaskOptions,
-            ILogger<LiveFansMedalAppService> logger,
-            IAccountDomainService accountDomainService
+            ILogger<LiveFansMedalAppService> logger
             )
         {
-            _configuration = configuration;
             _liveDomainService = liveDomainService;
-            _liveLotteryTaskOptions = liveLotteryTaskOptions.CurrentValue;
-            _securityOptions = securityOptions.CurrentValue;
             _logger = logger;
-            _accountDomainService = accountDomainService;
         }
 
         [TaskInterceptor("直播间互动", TaskLevel.One)]
         public override void DoTask()
         {
-            _liveDomainService.SendDanmakuToFansMedalLive();
-            _liveDomainService.LikeFansMedalLive();
-            _liveDomainService.SendHeartBeatToFansMedalLive();
+            SendDanmaku();
+            Like();
+            HeartBeat();
         }
 
+        [TaskInterceptor("发送弹幕", TaskLevel.Two,false)]
+        private void SendDanmaku()
+        {
+            _liveDomainService.SendDanmakuToFansMedalLive();
+        }
+
+        [TaskInterceptor("点赞直播间", TaskLevel.Two,false)]
+        private void Like()
+        {
+            _liveDomainService.LikeFansMedalLive();
+        }
+
+        [TaskInterceptor("直播时长挂机", TaskLevel.Two,false)]
+        private void HeartBeat()
+        {
+            _liveDomainService.SendHeartBeatToFansMedalLive();
+        }
     }
 }
