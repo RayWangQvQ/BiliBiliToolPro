d39fb447e6ad4c89ba5b7e238162c57464b01b5a
Ray <zai7lou@outlook.com>
2023-02-16 00:45:49 +0800

    update

--- qinglong/DefaultTasks/bili_task_base.sh
+++ qinglong/DefaultTasks/bili_task_base.sh
@@ -11,22 +11,26 @@ dotnetVersion=$(dotnet --version)
 if [[ $dotnetVersion == 6.* ]]; then
     echo "已安装dotnet，当前版本：$dotnetVersion"
 else
+    echo "which dotnet: $(which dotnet)"
     echo "开始安装dotnet"
     rayInstallShell="https://ghproxy.com/https://raw.githubusercontent.com/RayWangQvQ/BiliBiliToolPro/main/qinglong/ray-dotnet-install.sh"
     {
         echo "------尝试使用apk安装------"
         apk add dotnet6-sdk
         dotnet --version
+        echo "安装成功"
     } || {
         echo "------再尝试使用官方脚本安装------"
         curl -sSL $rayInstallShell | bash /dev/stdin
         . /root/.bashrc
         dotnet --version
+        echo "安装成功"
     } || {
         echo "------再尝试使用二进制包安装------"
         curl -sSL $rayInstallShell | bash /dev/stdin --no-official
         . /root/.bashrc
         dotnet --version
+        echo "安装成功"
     } || {
         echo "安装失败，没办法了，毁灭吧，自己解决吧：https://learn.microsoft.com/zh-cn/dotnet/core/install/linux-alpine"
         exit 1
