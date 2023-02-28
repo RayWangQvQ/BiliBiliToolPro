4810007ece249e2f974fce8620c760d6c4c6a509
Ray <zai7lou@outlook.com>
2023-02-28 21:17:04 +0800

    update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -19,93 +19,88 @@ version=""
 
 get_version() {
     version=$(grep -oP '(?<=<Version>).*?(?=<\/Version>)' $repoDir/common.props)
-    echo -e "current version: $version \n"
+    echo -e "current version: $version \n\n"
 }
 
 publish_dotnet_dependent() {
-    echo "---------start publishing dotnet dependent release---------"
-    cd $consoleDir
+    echo "---------start publishing 【dotnet dependent】 release---------"
 
+    echo "clear output dir"
+    local outputDir=$publishDir/dotnet-dependent
+    mkdir -p $outputDir
+    rm -rf $outputDir
+
+    cd $consoleDir
     echo "dotnet publish..."
-    dotnet publish --configuration Release --self-contained false -o $publishDir/dotnet-dependent
-    echo "dotnet Ray.BiliBiliTool.Console.dll" >$publishDir/dotnet-dependent/start.bat
+    dotnet publish --configuration Release \
+        --self-contained false \
+        -p:PublishSingleFile=true \
+        -o $outputDir
+    echo "dotnet Ray.BiliBiliTool.Console.dll" >$outputDir/start.bat
+
+    echo "clear pdb files"
+    rm -rf $outputDir/*.pdb
 
     echo "zip files..."
     cd $publishDir
     zip -q -r bilibili-tool-pro-v$version-dotnet-dependent.zip ./dotnet-dependent/*
     ls -l
-    echo -e "---------publish successfully---------\n"
+    echo -e "---------publish successfully---------\n\n"
 }
 
-publish_win() {
-    echo "---------start publishing win release---------"
-    cd $consoleDir
+publish_self_contained() {
+    local runtime=$1
+    echo "---------start publishing 【$runtime】 release---------"
 
+    echo "clear output dir"
+    local outputDir=$publishDir/$runtime
+    mkdir -p $outputDir
+    rm -rf $outputDir
+
+    cd $consoleDir
     echo "dotnet publish..."
-    dotnet publish --configuration Release --runtime win-x86 --self-contained true -p:PublishTrimmed=true -o $publishDir/win-x86-x64
+    dotnet publish --configuration Release \
+        --self-contained true \
+        --runtime $runtime \
+        -p:PublishTrimmed=true \
+        -p:PublishSingleFile=true \
+        -o $outputDir
+
+    echo "clear pdb files"
+    rm -rf $outputDir/*.pdb
 
     echo "zip files..."
     cd $publishDir
-    zip -q -r bilibili-tool-pro-v$version-win-x86-x64.zip ./win-x86-x64/*
-    echo -e "---------publish successfully---------\n"
-}
-
-publish_linux_arm() {
-    echo "---------start publishing linux arm release---------"
-    cd $consoleDir
-    
-    echo "dotnet publish..."
-    dotnet publish --configuration Release --runtime linux-arm --self-contained true -p:PublishTrimmed=true -o $publishDir/linux-arm
-
-    echo "zip files..."
-    cd $publishDir
-    zip -q -r bilibili-tool-pro-v$version-linux-arm.zip ./linux-arm/*
-    echo -e "---------publish successfully---------\n"
-}
-
-publish_linux_x64() {
-    echo "---------start publishing linux x64 release---------"
-    cd $consoleDir
-
-    echo "dotnet publish..."
-    dotnet publish --configuration Release --runtime linux-x64 --self-contained true -p:PublishTrimmed=true -o $publishDir/linux-x64
-
-    echo "zip files..."
-    cd $publishDir
-    zip -q -r bilibili-tool-pro-v$version-linux-x64.zip ./linux-x64/*
-    echo -e "---------publish successfully---------\n"
-}
-
-publish_osx_x64() {
-    echo "---------start publishing osx x64 release---------"
-    cd $consoleDir
-
-    echo "dotnet publish..."
-    dotnet publish --configuration Release --runtime osx-x64 --self-contained true -p:PublishTrimmed=true -o $publishDir/osx-x64
-
-    echo "zip files..."
-    cd $publishDir
-    zip -q -r bilibili-tool-pro-v$version-osx-x64.zip ./osx-x64/*
-    echo -e "---------publish successfully---------\n"
+    zip -q -r bilibili-tool-pro-v$version-$runtime.zip $publishDir/$runtime/*
+    ls -l
+    echo -e "---------publish successfully---------\n\n"
 }
 
 publish_tencentScf() {
-    echo "---------start publishing linux x64 release---------"
+    echo "---------start publishing 【tencent scf】 release---------"
     cd $publishDir
     cp -r $repoDir/tencentScf/bootstrap $repoDir/tencentScf/index.sh ./linux-x64/
     cd ./linux-x64
     chmod 755 index.sh bootstrap
     zip -q -r ../bilibili-tool-pro-v$version-tencent-scf.zip ./*
-    echo -e "---------publish successfully---------\n"
+    cd .. && ls
+    echo -e "---------publish successfully---------\n\n"
 }
 
 main() {
     get_version
+
+    # dotnet dependent
     publish_dotnet_dependent
-    publish_win
-    publish_linux_arm
-    publish_linux_x64
-    publish_osx_x64
+
+    # self contained
+    # https://learn.microsoft.com/zh-cn/dotnet/core/rid-catalog
+    array=("win-x86" "win-x64" "win-arm64" "linux-x64" "linux-musl-x64" "linux-arm64" "linux-arm" "osx-x64")
+    for i in "${array[@]}"; do
+        echo "$i"
+        publish_self_contained $i
+    done
+
     publish_tencentScf
 }
 
