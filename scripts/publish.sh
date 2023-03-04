bf2a5b1a90f21ec7dc9500e12abfbf7b463c373d
Ray <zai7lou@outlook.com>
2023-03-04 18:15:00 +0800

    feat: update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -15,7 +15,34 @@ repoDir=$(dirname $PWD)
 consoleDir=$repoDir/src/Ray.BiliBiliTool.Console
 publishDir=$consoleDir/bin/Publish
 version=""
+runTime=""
 # --------------------------
+
+read_params_from_init_cmd() {
+    while [ $# -ne 0 ]; do
+        name="$1"
+        case "$name" in
+        -r | --runtime | -[Rr]untime)
+            shift
+            runTime="$1"
+            ;;
+        *)
+            say_err "Unknown argument \`$name\`"
+            exit 1
+            ;;
+        esac
+        shift
+    done
+}
+
+read_var_from_user() {
+    # runTime
+    if [ -z "$runTime" ]; then
+        read -p 'please input runTime("all" "win-x86" "win-x64" "win-arm64" "linux-x64" "linux-musl-x64" "linux-arm64" "linux-arm" "osx-x64")' runTime
+    else
+        echo "runTime: $runTime"
+    fi
+}
 
 get_version() {
     version=$(grep -oP '(?<=<Version>).*?(?=<\/Version>)' $repoDir/common.props)
@@ -87,6 +114,9 @@ publish_tencentScf() {
 }
 
 main() {
+    read_params_from_init_cmd $*
+    read_var_from_user
+
     get_version
 
     # dotnet dependent
@@ -95,12 +125,16 @@ main() {
     # self contained
     # https://learn.microsoft.com/zh-cn/dotnet/core/rid-catalog
     array=("win-x86" "win-x64" "win-arm64" "linux-x64" "linux-musl-x64" "linux-arm64" "linux-arm" "osx-x64")
+    if [ "$runTime" != "all" ]; then
+        array=("$runTime")
+    fi
     for i in "${array[@]}"; do
-        echo "$i"
         publish_self_contained $i
     done
 
-    publish_tencentScf
+    if [ "$runTime" == "all" ]; then
+        publish_tencentScf
+    fi
 }
 
-main
+main $*
