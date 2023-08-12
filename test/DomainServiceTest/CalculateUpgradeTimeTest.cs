ef6ea123740f435259c3f42b2457dd01fe2bd7b5
Polaris_cn <2984098720@qq.com>
2023-08-12 21:35:46 +0800

    基本完成升级时间计算的升级，并增加一个单元测试 (#590)
    * 基本完成升级时间计算的升级，并增加一个单元测试。
    
    * 调整了升级时间的日志描述

---
+++ test/DomainServiceTest/CalculateUpgradeTimeTest.cs
@@ -0,0 +1,48 @@
+﻿using Ray.BiliBiliTool.Agent.BiliBiliAgent.Dtos;
+
+namespace DomainServiceTest;
+
+public class CalculateUpgradeTimeTest
+{
+    public CalculateUpgradeTimeTest()
+    {
+        Program.CreateHost(new[] { "--ENVIRONMENT=Development" });
+    }
+
+    [Fact]
+    public void TestCalculateUpgradeTime()
+    {
+        using var scope = Global.ServiceProviderRoot.CreateScope();
+        var accountDomainService = scope.ServiceProvider.GetRequiredService<IAccountDomainService>();
+        int needDay = accountDomainService.CalculateUpgradeTime(new UserInfo()
+        {
+            Money = 7,
+            Level_info = new LevelInfo()
+            {
+                Current_level = 5,
+                Current_exp = 100,
+                Next_exp = 200
+            }
+        });
+        int needDay2 = accountDomainService.CalculateUpgradeTime(new UserInfo()
+        {
+            Money = 7,
+            Level_info = new LevelInfo()
+            {
+                Current_level = 5,
+                Current_exp = 1000,
+                Next_exp = 2000
+            }
+        });
+
+        Assert.Equal(1,needDay);
+        Assert.Equal(37,needDay2);
+
+    }
+}
+
+
+
+
+
+
