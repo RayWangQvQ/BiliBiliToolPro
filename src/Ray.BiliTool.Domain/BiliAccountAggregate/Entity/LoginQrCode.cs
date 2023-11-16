ede34084ddb4bb0ce6513d8f737ea368eae35f76
Ray <zai7lou@outlook.com>
2023-11-17 00:19:11 +0800

    update

---
+++ src/Ray.BiliTool.Domain/BiliAccountAggregate/Entity/LoginQrCode.cs
@@ -0,0 +1,17 @@
+﻿using Ray.DDD;
+using SixLabors.ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ray.BiliTool.Domain.BiliAccountAggregate.Entity
+{
+    public class LoginQrCode
+    {
+        public string CodeKey { get; set; }
+
+        public string CodeInBase64 { get; set; }
+    }
+}
--- src/Ray.BiliTool.Domain/BiliAccountAggregate/Service/BiliAccountService.cs
+++ src/Ray.BiliTool.Domain/BiliAccountAggregate/Service/BiliAccountService.cs
@@ -4,19 +4,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ray.BiliBiliTool.Agent.BiliBiliAgent.Dtos;
+using Ray.BiliBiliTool.Agent.BiliBiliAgent.Dtos.Passport;
+using Ray.BiliTool.Domain.BiliAccountAggregate.Entity;
 using Ray.DDD;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using ZXing.QrCode.Internal;
+using Ray.BiliBiliTool.Agent.BiliBiliAgent.Interfaces;
+using Ray.Infrastructure.BarCode;
 
 namespace Ray.BiliTool.Domain.BiliAccountAggregate.Service
 {
     public class BiliAccountService: IDomainService
     {
         private readonly IBiliAccountRepository _repository;
+        private readonly IPassportApi _passportApi;
 
         public BiliAccountService(
-            IBiliAccountRepository repository
+            IBiliAccountRepository repository,
+            IPassportApi passportApi
             )
         {
             _repository = repository;
+            _passportApi = passportApi;
+        }
+
+        public async Task<LoginQrCode> GenerateLoginQrCodeAsync()
+        {
+            BiliApiResponse<QrCodeDto> re = await _passportApi.GenerateQrCode();
+            if (re.Code != 0)
+            {
+                ModalService.Error(new ConfirmOptions()
+                {
+                    Title = "获取二维码失败",
+                    Content = re.ToJsonStr()
+                });
+                return;
+            }
+            var url = re.Data.Url;
+
+            Image<Rgba32>? bitmap = BarCodeHelper.EncodeByImageSharp(url);
+
+            _qrCode = bitmap.ToBase64String(PngFormat.Instance);
+            _qrCodeKey = re.Data.Qrcode_key;
         }
 
         public void LoginByScan()
