ff88a7ec7031a4e338113dca939b356780e81866
Ray <zai7lou@outlook.com>
2024-05-11 14:59:16 +0800

    test:[qinglong]test cron

--- qinglong/DefaultTasks/dev/bili_dev_task_daily.sh
+++ qinglong/DefaultTasks/dev/bili_dev_task_daily.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-# 0 9 * * * bili_dev_task_daily.sh
-# new Env("bili每日任务[dev先行版]")
+#0 9 * * * bili_dev_task_daily.sh
+#new Env("bili每日任务[dev先行版]");
+#
 
 . bili_dev_task_base.sh
 
