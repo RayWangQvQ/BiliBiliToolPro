6feacf714c48a09d725f931c08c7055ae4bad85c
Ray <zai7lou@outlook.com>
2024-05-22 22:47:32 +0800

    update UT script

--- scripts/ut.ps1
+++ scripts/ut.ps1
@@ -6,12 +6,14 @@ dotnet tool install -g dotnet-reportgenerator-globaltool
 
 # 运行单元测试并生成覆盖率报告
 Write-Output "Running unit tests and generating coverage report..."
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=./TestResults/coverage.opencover.xml
 
-# 生成 HTML 格式的覆盖率报告
-Write-Output "Generating HTML coverage report..."
-reportgenerator "-reports:TestResults/*/coverage.opencover.xml" "-targetdir:coveragereport" -reporttypes:Html
+# 生成html报告
+$coverageFiles = Get-ChildItem -Path . -Recurse -Filter "coverage.cobertura.xml"
+$coverageFiles | ForEach-Object { Write-Output $_.FullName }
+$reportPaths = ($coverageFiles | ForEach-Object { $_.FullName }) -join ";"
+reportgenerator "-reports:$reportPaths" "-targetdir:coveragereport" -reporttypes:Html
 
-# 打开生成的覆盖率报告
-Write-Output "Opening coverage report..."
+# 检查生成的覆盖率报告文件是否存在
+Write-Output "Coverage report generated successfully."
 Start-Process "coveragereport/index.htm"
