bfb615783f2692f9a8f65928d965b8d9be067243
Ray <zai7lou@outlook.com>
2025-05-18 00:22:23 +0800

    update docker-compose and install scripts for improved container management and logging

--- docker/install.sh
+++ docker/install.sh
@@ -11,30 +11,224 @@ set -e
 set -u
 set -o pipefail
 
-echo '  ____               ____    _   _____           _  '
-echo ' |  _ \ __ _ _   _  | __ ) _| |_|_   _|__   ___ | | '
-echo ' | |_) / _` | | | | |  _ \(_) (_) | |/ _ \ / _ \| | '
-echo ' |  _ < (_| | |_| | | |_) | | | | | | (_) | (_) | | '
-echo ' |_| \_\__,_|\__, | |____/|_|_|_| |_|\___/ \___/|_| '
-echo '             |___/                                  '
+echo '  ____    _   _____           _  '
+echo ' | __ ) _| |_|_   _|__   ___ | | '
+echo ' |  _ \(_) (_) | |/ _ \ / _ \| | '
+echo ' | |_) | | | | | | (_) | (_) | | '
+echo ' |____/|_|_|_| |_|\___/ \___/|_| '
 
-githubProxy="https://ghproxy.com/"
+current_dir=$(pwd)
+base_dir="${current_dir}/bili_web"
+github_proxy=""
+github_branch="main"
+remote_compose_url="${github_proxy}https://raw.githubusercontent.com/RayWangQvQ/BiliBiliToolPro/refs/heads/${github_branch}/docker/sample/docker-compose.yml"
+remote_ckJson_url="${github_proxy}https://raw.githubusercontent.com/RayWangQvQ/BiliBiliToolPro/refs/heads/${github_branch}/docker/sample/cookies.json"
+docker_img_name="ghcr.io/raywangqvq/bili_tool_web"
+container_name="bili_tool_web"
 
-infFileName="ray-inf.sh"
-[ -f $infFileName ] || {
-    infUrl="${githubProxy}https://raw.githubusercontent.com/RayWangQvQ/ray-sh/main/$infFileName"
-    (wget $infUrl) || (curl -sSL -f --create-dirs $infUrl) || {
-        echo "未找到 wget 或 curl 命令，请自行安装"
-        exit 1
-    }
+### infra
+verbose=false
+
+invocation='echo "" && say_verbose "Calling: ${yellow:-}${FUNCNAME[0]} ${green:-}$*${normal:-}"'
+
+if [ -t 1 ] && command -v tput >/dev/null; then
+    ncolors=$(tput colors || echo 0)
+    if [ -n "$ncolors" ] && [ $ncolors -ge 8 ]; then
+        bold="$(tput bold || echo)"
+        normal="$(tput sgr0 || echo)"
+        black="$(tput setaf 0 || echo)"
+        red="$(tput setaf 1 || echo)"
+        green="$(tput setaf 2 || echo)"
+        yellow="$(tput setaf 3 || echo)"
+        blue="$(tput setaf 4 || echo)"
+        magenta="$(tput setaf 5 || echo)"
+        cyan="$(tput setaf 6 || echo)"
+        white="$(tput setaf 7 || echo)"
+    fi
+fi
+
+say_verbose() {
+    if [ "$verbose" = true ]; then
+        # using stream 3 (defined in the beginning) to not interfere with stdout of functions
+        # which may be used as return value
+        printf "%b\n" "${cyan:-}$(date "+%Y-%m-%d %H:%M:%S")[VER]:${normal:-} $1" >&3
+    fi
 }
-. ray-inf.sh
 
-base_dir="/bili"
-remote_compose_url="${githubProxy}https://raw.githubusercontent.com/RayWangQvQ/BiliBiliToolPro/main/docker/sample/docker-compose.yml"
-remote_config_url="${githubProxy}https://raw.githubusercontent.com/RayWangQvQ/BiliBiliToolPro/main/src/Ray.BiliBiliTool.Console/appsettings.json"
-remote_ckJson_url="${githubProxy}https://raw.githubusercontent.com/RayWangQvQ/BiliBiliToolPro/main/docker/sample/cookies.json"
-docker_img_name="ghcr.io/raywangqvq/bilibili_tool_pro"
+say_info() {
+    printf "%b\n" "${green:-}$(date "+%Y-%m-%d %H:%M:%S")[INF]:$1${normal:-}" >&2
+}
+
+say_warning() {
+    printf "%b\n" "${yellow:-}$(date "+%Y-%m-%d %H:%M:%S")[WAR]:$1${normal:-}" >&3
+}
+
+say_err() {
+    printf "%b\n" "${red:-}$(date "+%Y-%m-%d %H:%M:%S")[ERR]:$1${normal:-}" >&2
+}
+
+machine_has() {
+    eval $invocation
+
+    command -v "$1" >/dev/null 2>&1
+    return $?
+}
+
+# args:
+# remote_path - $1
+get_http_header_curl() {
+    eval $invocation
+
+    local remote_path="$1"
+
+    curl_options="-I -sSL --retry 5 --retry-delay 2 --connect-timeout 15 "
+    curl $curl_options "$remote_path" 2>&1 || return 1
+    return 0
+}
+
+# args:
+# remote_path - $1
+get_http_header_wget() {
+    eval $invocation
+
+    local remote_path="$1"
+    local wget_options="-q -S --spider --tries 5 "
+    # Store options that aren't supported on all wget implementations separately.
+    local wget_options_extra="--waitretry 2 --connect-timeout 15 "
+    local wget_result=''
+
+    wget $wget_options $wget_options_extra "$remote_path" 2>&1
+    wget_result=$?
+
+    if [[ $wget_result == 2 ]]; then
+        # Parsing of the command has failed. Exclude potentially unrecognized options and retry.
+        wget $wget_options "$remote_path" 2>&1
+        return $?
+    fi
+
+    return $wget_result
+}
+
+# Updates global variables $http_code and $download_error_msg
+downloadcurl() {
+    eval $invocation
+
+    unset http_code
+    unset download_error_msg
+    local remote_path="$1"
+    local out_path="${2:-}"
+    local remote_path_with_credential="${remote_path}"
+    local curl_options="--retry 20 --retry-delay 2 --connect-timeout 15 -sSL -f --create-dirs "
+    local failed=false
+    if [ -z "$out_path" ]; then
+        curl $curl_options "$remote_path_with_credential" 2>&1 || failed=true
+    else
+        curl $curl_options -o "$out_path" "$remote_path_with_credential" 2>&1 || failed=true
+    fi
+    if [ "$failed" = true ]; then
+        local response=$(get_http_header_curl $remote_path)
+        http_code=$(echo "$response" | awk '/^HTTP/{print $2}' | tail -1)
+        download_error_msg="Unable to download $remote_path."
+        if [[ $http_code != 2* ]]; then
+            download_error_msg+=" Returned HTTP status code: $http_code."
+        fi
+        say_verbose "$download_error_msg"
+        return 1
+    fi
+    return 0
+}
+
+# Updates global variables $http_code and $download_error_msg
+downloadwget() {
+    eval $invocation
+
+    unset http_code
+    unset download_error_msg
+    local remote_path="$1"
+    local out_path="${2:-}"
+    local remote_path_with_credential="${remote_path}"
+    local wget_options="--tries 20 "
+    # Store options that aren't supported on all wget implementations separately.
+    local wget_options_extra="--waitretry 2 --connect-timeout 15 "
+    local wget_result=''
+
+    if [ -z "$out_path" ]; then
+        wget -q $wget_options $wget_options_extra -O - "$remote_path_with_credential" 2>&1
+        wget_result=$?
+    else
+        wget $wget_options $wget_options_extra -O "$out_path" "$remote_path_with_credential" 2>&1
+        wget_result=$?
+    fi
+
+    if [[ $wget_result == 2 ]]; then
+        # Parsing of the command has failed. Exclude potentially unrecognized options and retry.
+        if [ -z "$out_path" ]; then
+            wget -q $wget_options -O - "$remote_path_with_credential" 2>&1
+            wget_result=$?
+        else
+            wget $wget_options -O "$out_path" "$remote_path_with_credential" 2>&1
+            wget_result=$?
+        fi
+    fi
+
+    if [[ $wget_result != 0 ]]; then
+        local disable_feed_credential=false
+        local response=$(get_http_header_wget $remote_path $disable_feed_credential)
+        http_code=$(echo "$response" | awk '/^  HTTP/{print $2}' | tail -1)
+        download_error_msg="Unable to download $remote_path."
+        if [[ $http_code != 2* ]]; then
+            download_error_msg+=" Returned HTTP status code: $http_code."
+        fi
+        say_verbose "$download_error_msg"
+        return 1
+    fi
+
+    return 0
+}
+
+# args:
+# remote_path - $1
+# [out_path] - $2 - stdout if not provided
+download() {
+    eval $invocation
+
+    local remote_path="$1"
+    local out_path="${2:-}"
+
+    if [[ "$remote_path" != "http"* ]]; then
+        cp "$remote_path" "$out_path"
+        return $?
+    fi
+
+    local failed=false
+    local attempts=0
+    while [ $attempts -lt 3 ]; do
+        attempts=$((attempts + 1))
+        failed=false
+        if machine_has "curl"; then
+            downloadcurl "$remote_path" "$out_path" || failed=true
+        elif machine_has "wget"; then
+            downloadwget "$remote_path" "$out_path" || failed=true
+        else
+            say_err "Missing dependency: neither curl nor wget was found."
+            exit 1
+        fi
+
+        if [ "$failed" = false ] || [ $attempts -ge 3 ] || { [ ! -z $http_code ] && [ $http_code = "404" ]; }; then
+            break
+        fi
+
+        say "Download attempt #$attempts has failed: $http_code $download_error_msg"
+        say "Attempt #$((attempts + 1)) will start in $((attempts * 10)) seconds."
+        sleep $((attempts * 10))
+    done
+
+    if [ "$failed" = true ]; then
+        say_verbose "Download failed: $remote_path"
+        return 1
+    fi
+    return 0
+}
 
 createBaseDir() {
     eval $invocation
@@ -72,11 +266,9 @@ downloadResources() {
     # docker compose
     [ -f "docker-compose.yml" ] || download $remote_compose_url ./docker-compose.yml
 
-    # config
-    [ -f "appsettings.json" ] || download $remote_config_url ./appsettings.json
-
     # ckJson
     [ -f "cookies.json" ] || download $remote_ckJson_url ./cookies.json
+    chmod +x ./cookies.json
 
     ls -l
 }
@@ -93,9 +285,9 @@ runContainer() {
     } || {
         docker-compose version && docker-compose up -d
     } || {
-        docker run -d --name="bili" \
+        docker run -d --name="${container_name}" \
+            -p 22330:8080 \
             -v $base_dir/Logs:/app/Logs \
-            -v $base_dir/appsettings.json:/app/appsettings.json \
             -v $base_dir/cookies.json:/app/cookies.json \
             $docker_img_name
     } || {
@@ -108,25 +300,25 @@ checkResult() {
     eval $invocation
     say_info "检测容器运行情况"
 
-    local containerName="bili"
-    docker ps --filter "name=$containerName"
+    docker ps --filter "name=${container_name}"
 
-    containerId=$(docker ps -q --filter "name=^$containerName$")
+    containerId=$(docker ps -q --filter "name=^${container_name}$")
     if [ -n "$containerId" ]; then
         docker logs bili
         echo ""
         echo "==============================================="
         echo "Congratulations! 恭喜！"
-        echo "创建并运行$containerName容器成功。"
-        echo "第一次运行，请执行扫码登录："
-        echo "docker exec -it $containerName dotnet /app/Ray.BiliBiliTool.Console.dll --runTasks=Login --Security__RandomSleepMaxMin=0"
+        echo "创建并运行${container_name}容器成功。"
+        echo "访问地址：http:{ip}:22330"
+        echo "云服务器防火墙请自行开放22330端口"
+        echo "首次运行后，请执行扫码登录任务添加账号"
         echo "Enjoy it~"
         echo "==============================================="
     else
         echo ""
         echo "请查看运行日志，确认容器是否正常运行，点击 Ctrl+c 退出日志追踪"
         echo ""
-        docker logs -f $containerName
+        docker logs -f ${container_name}
     fi
 }
 
