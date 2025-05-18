c5c203e20d0f46f0173c92584c08b3f13161225b
Ray <zai7lou@outlook.com>
2025-05-18 20:48:15 +0800

    update scripts for qinglong dotnet version checks

--- qinglong/DefaultTasks/bili_task_tryFix.sh
+++ qinglong/DefaultTasks/bili_task_tryFix.sh
@@ -7,21 +7,24 @@ dir_shell=$QL_DIR/shell
 . /root/.bashrc
 
 bili_repo="raywangqvq_bilibilitoolpro"
+bili_branch=""
 
-echo -e "\nrepo目录: $dir_repo"
-bili_repo_dir="$(find $dir_repo -type d -iname $bili_repo | head -1)"
-echo -e "bili仓库目录: $bili_repo_dir\n"
+echo "青龙repo目录: $dir_repo"
+qinglong_bili_repo="$(echo "$bili_repo" | sed 's/\//_/g')${bili_branch}"
+qinglong_bili_repo_dir="$(find $dir_repo -type d \( -iname $qinglong_bili_repo -o -iname ${qinglong_bili_repo}_main \) | head -1)"
+echo "bili仓库目录: $qinglong_bili_repo_dir"
 
 echo -e "清理缓存...\n"
-cd $bili_repo_dir
+cd $qinglong_bili_repo_dir
 find . -type d -name "bin" -exec rm -rf {} +
 find . -type d -name "obj" -exec rm -rf {} +
 echo -e "清理完成\n"
 
 echo "检测dotnet..."
 dotnetVersion=$(dotnet --version)
-if [[ $dotnetVersion == 6.* ]]; then
-    echo "已安装dotnet，当前版本：$dotnetVersion"
+echo "当前dotnet版本：$dotnetVersion"
+if [[ $(echo "$dotnetVersion" | grep -oE '^[0-9]+') -ge 8 ]]; then
+    echo "已安装，且版本满足"
 else
     echo "which dotnet: $(which dotnet)"
     echo "Paht: $PATH"
