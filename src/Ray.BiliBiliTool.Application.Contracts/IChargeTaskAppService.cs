e675adb31d1363ae874584d7349ffa0f97e2f7d1
Ray <zai7lou@outlook.com>
2025-06-21 13:39:47 +0800

    fix typo

--- src/Ray.BiliBiliTool.Application.Contracts/IChargeTaskAppService.cs
+++ src/Ray.BiliBiliTool.Application.Contracts/IChargeTaskAppService.cs
@@ -3,7 +3,7 @@
 namespace Ray.BiliBiliTool.Application.Contracts;
 
 /// <summary>
-/// 漫画任务
+/// 免费B币券充电任务
 /// </summary>
 [Description("Charge")]
 public interface IChargeTaskAppService : IAppService;
