26d4a799a8b97b20ae505e730f88bc9ce0c67cee
Ray <zai7lou@outlook.com>
2025-07-13 16:44:51 +0800

    Update entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,13 +2,13 @@
 set -e
 
 echo "Starting BiliTool container..."
+mkdir -p /app/config
 
 echo "Running maintenance scripts..."
 
 # 3.3.0 need migrate db file location to /app/config
 if [ -f "/app/BiliBiliTool.db" ]; then
     echo "[3.3.0]Migrate db file location to /app/config"
-    mkdir -p /app/config
     mv /app/BiliBiliTool.db /app/config/BiliBiliTool.db
 fi
 
