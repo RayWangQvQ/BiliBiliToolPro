bdcbe0e9dfe8c619f41fc19bb8cf05fee4d2f7f5
Ray <zai7lou@outlook.com>
2025-07-13 16:58:09 +0800

    Format shell scripts

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,7 +9,7 @@ echo "Running maintenance scripts..."
 
 # 3.3.0 need migrate db file location to /app/config
 if [ -f "/app/BiliBiliTool.db" ]; then
-    echo "[3.3.0]Migrate db file location to /app/config"
+    echo "[3.3.0] Migrate db file location to /app/config"
     mv /app/BiliBiliTool.db /app/config/BiliBiliTool.db
 fi
 
